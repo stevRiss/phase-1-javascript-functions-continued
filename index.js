cf05df696aabbc3b8019543979ecfbb785380094
Stephen <stevriss22@gmail.com>
2021-09-21 10:42:28 -0400

    commit tree e569c5248245b0f3e3c3916963a73d0e4563a118

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+
+
+function wrapAdjective(flare="*"){
+
+    // let special = "special";
+    const innFunc = function adjective(special="special"){
+        //flare = "*";
+        return `You are ${flare}${special}${flare}!`;
+   }
+   return innFunc;
+}
+console.log(wrapAdjective()());
+
+console.log(wrapAdjective("*")("a hardworker"));
+console.log(wrapAdjective("||")("a dedicated programmer"));
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun("game"));
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+
+}
